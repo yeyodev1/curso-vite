9bb41cdad68894f91f168ca413c217e8d3c05a33
Diego Reyes <diegorele13@gmail.com>
2022-10-04 15:33:55 -0500

    first commit

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ document.querySelector("#app").innerHTML = `
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-
-
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
